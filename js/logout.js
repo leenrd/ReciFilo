86fd4f62c390d8da8de65391026df7fed5647076
Leenard <lolleenard0@gmail.com>
2023-06-23 13:49:38 +0800

    Upload Finished

--- js/logout.js
+++ js/logout.js
@@ -2,6 +2,7 @@ const logoutBtn = document.getElementById("logOutBtn").onclick = (() => {
     console.log("logout");
     firebase.auth().signOut().then(() => {
         sessionStorage.removeItem("uid");
+        localStorage.clear();
         alert('Logged Out!')
         window.location.href = "../src/LogInPage.html";
     })
--- js/search.js
+++ js/search.js
@@ -1,23 +1,30 @@
-const sectionResults = document.getElementById("searchResults")
+const sectionResults = document.getElementById("searchResults");
 const searchBtn = document.getElementById("searchBtn");
-
-
+const db = firebase.firestore();
+const uid = sessionStorage.getItem('uid');
+const bookmarksCollection = db.collection("users").doc(uid).collection("bookmarks");
 
 searchBtn.addEventListener("click", () => {
     console.log("clickedSearch");
-    apiReq()
-})
+    apiReq();
+});
 
 const apiReq = async () => {
-    const searchInput = document.getElementById("searchBar")
+    const searchInput = document.getElementById("searchBar");
     const APP_ID = "a7bcdd71";
-    let API_KEY = "2f3291ec2a896239f90605d217897ca3"
+    const API_KEY = "2f3291ec2a896239f90605d217897ca3";
     let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${searchInput.value}`);
     let data = await response.json();
     let recipes = data.hits;
-    sectionResults.innerHTML = " "
+
+    if (recipes.length === 0) {
+        sectionResults.innerHTML = "<p>No Dishes found...</p>";
+    } else {
+        sectionResults.innerHTML = "";
+    }
+
     recipes.forEach((item) => {
-        let rep = item.recipe
+        let rep = item.recipe;
         let card = document.createElement("div");
         card.className = "card";
         card.innerHTML = `
@@ -30,52 +37,79 @@ const apiReq = async () => {
             </div>
         `;
         sectionResults.appendChild(card);
-    })
 
-    const bookmarkBtns = document.querySelectorAll(".bookmark-btn");
-
-    bookmarkBtns.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
-        const card = event.target.closest(".card");
         const bookmarkBtn = card.querySelector(".bookmark-btn");
-        const isBookmarked = card.classList.toggle("bookmarked");
+        const recipeId = item.recipe.uri;
         const recipeCardHTML = card.outerHTML;
 
-        if (isBookmarked) {
-        bookmarkBtn.innerText = "Bookmarked";
-        addBookmark(recipeCardHTML);
-        console.log("Bookmark added");
-        } else {
-        bookmarkBtn.innerText = "Bookmark";
-        removeBookmark(recipeCardHTML);
-        console.log("Bookmark removed");
-        }
-    });
-    });
+        bookmarkBtn.addEventListener("click", () => {
+            const isBookmarked = card.classList.toggle("bookmarked");
 
+            if (isBookmarked) {
+                bookmarkBtn.innerText = "Bookmarked";
+                addBookmark(recipeId, recipeCardHTML);
+                console.log("Bookmark added");
+            } else {
+                bookmarkBtn.innerText = "Bookmark";
+                removeBookmark(recipeId, recipeCardHTML);
+                console.log("Bookmark removed");
+            }
+        });
+    });
+};
 
-    function addBookmark(recipeCardHTML) {
-        const bookmarks = getBookmarks();
-        bookmarks.push(recipeCardHTML);
-        saveBookmarks(bookmarks);
-      }
-      function removeBookmark(recipeCardHTML) {
-        const bookmarks = getBookmarks();
-        const index = bookmarks.indexOf(recipeCardHTML);
-        if (index !== -1) {
-          bookmarks.splice(index, 1);
-          saveBookmarks(bookmarks);
-        }
-      }
-      function getBookmarks() {
-        const bookmarksJSON = localStorage.getItem("bookmarks");
-        return bookmarksJSON ? JSON.parse(bookmarksJSON) : [];
-      }
-      function saveBookmarks(bookmarks) {
-        localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
-      }
+function removeBookmark(recipeId) {
+    bookmarksCollection
+        .where("recipeId", "==", recipeId)
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                doc.ref
+                    .delete()
+                    .then(() => {
+                        console.log("Bookmark removed");
+                    })
+                    .catch((error) => {
+                        console.error("Error removing bookmark: ", error);
+                    });
+            });
+        })
+        .catch((error) => {
+            console.error("Error getting bookmark: ", error);
+        });
 }
 
+function getBookmarks() {
+    return bookmarksCollection
+        .get()
+        .then((querySnapshot) => {
+            const bookmarks = [];
+            querySnapshot.forEach((doc) => {
+                bookmarks.push(doc.data().recipeId);
+            });
+            return bookmarks;
+        })
+        .catch((error) => {
+            console.error("Error getting bookmarks: ", error);
+            return [];
+        });
+}
 
+function addBookmark(recipeId, recipeCardHTML) {
+    bookmarksCollection
+        .add({
+            recipeId: recipeId,
+            recipeCardHTML: recipeCardHTML
+        })
+        .then((docRef) => {
+            console.log("Bookmark added with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding bookmark: ", error);
+        });
+}
 
+// Retrieve and use the bookmarks for the current user
+getBookmarks().then((bookmarks) => {
 
+});
